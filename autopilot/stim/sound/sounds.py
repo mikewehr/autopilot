7bdf506534e229d7d271c6f9b3fa51aaa4959b97
Mike Wehr <wehr@uoregon.edu>
2022-02-18 13:56:29 -0800

    more ramp debugging

--- autopilot/stim/sound/sounds.py
+++ autopilot/stim/sound/sounds.py
@@ -115,7 +115,7 @@ class Tone(BASE_CLASS):
         dt=1/self.fs
         t=np.arange(dt, np.pi/2/omega + dt, dt)
         Redge=(np.cos(omega*t))**2
-        self.logger.debug(f"redge {shape(Redge)}")
+        self.logger.debug(f"redge {np.shape(Redge)}")
         Ledge=np.fliplr(Redge)
         self.table[0:len(Ledge)-1] = self.table[0:len(Ledge)-1]*Ledge
         self.table[(len(self.table)-len(Redge)):len(self.table)-1] = self.table[(len(self.table)-len(Redge)):len(self.table)-1]*Redge
