2145a36e6edf2484803357dd4f04f79afa9e76ff
Mike Wehr <wehr@uoregon.edu>
2022-02-18 13:44:26 -0800

    more ramp debugging[A

--- autopilot/stim/sound/sounds.py
+++ autopilot/stim/sound/sounds.py
@@ -112,6 +112,7 @@ class Tone(BASE_CLASS):
         #pdb.set_trace()
         self.logger.debug('applying ramp...')
         omega = (1000/self.ramp)*(math.acos(math.sqrt(0.1)) - math.acos(math.sqrt(0.9)))
+        self.logger.debug(f'omega')
         dt=1/self.fs
         t=np.arange(dt, np.pi/2/omega + dt, dt)
         Redge=(math.cos(omega*t))**2
