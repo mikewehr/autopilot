c02a7ab955a255b3f3957ddd66d093019feecff8
Mike Wehr <wehr@uoregon.edu>
2022-02-21 13:36:39 -0800

    more ramp debugging

--- autopilot/stim/sound/sounds.py
+++ autopilot/stim/sound/sounds.py
@@ -109,7 +109,7 @@ class Tone(BASE_CLASS):
         generate a rising/falling cosine-squared edge and apply it to the tone
         ramp (in ms) is the duration from 10% to 90% of tone amplitude
         """
-        self.logger.debug('applying ramp...')
+        self.logger.debug(f'applying {ramp} ms ramp...')
         omega = (1000/self.ramp)*(math.acos(math.sqrt(0.1)) - math.acos(math.sqrt(0.9)))
         self.logger.debug(f"omega:  {omega}")
         dt=1/self.fs
@@ -117,7 +117,7 @@ class Tone(BASE_CLASS):
         Redge=(np.cos(omega*t))**2
         Ledge=np.flipud(Redge)
         self.logger.debug(f"len Ledge  {len(Ledge)}")
-        pdb.set_trace()
+        #pdb.set_trace()
         self.table[0:len(Ledge)] = self.table[0:len(Ledge)]*Ledge
         self.table[(len(self.table)-len(Redge)):len(self.table)] = self.table[(len(self.table)-len(Redge)):len(self.table)]*Redge
         self.logger.debug('ramp applied')
